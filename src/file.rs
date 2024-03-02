e2080ec7b1ccc7cee84594f2104e0c0c621141b5
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-03-02 16:18:12 +0000

    add naive fileblock struct to file module

--- src/file.rs
+++ src/file.rs
@@ -1,1 +1,17 @@
-//! TODO:
+use core::ops::Deref;
+
+pub struct FileBlock(pub Vec<u8>);
+
+impl Deref for FileBlock {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<&[u8]> for FileBlock {
+    fn from(value: &[u8]) -> Self {
+        FileBlock(value.into())
+    }
+}
