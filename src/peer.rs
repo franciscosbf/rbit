c01b8c752862985097e14ed3ed7640b2abaa8705
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-03-02 16:18:59 +0000

    add base structure to peer client+server and refactor message encoding/decoding

--- src/peer.rs
+++ src/peer.rs
@@ -1,4 +1,21 @@
-use std::ops::Deref;
+// TODO: remove
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::{
+    ops::Deref,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, RwLock,
+    },
+};
+
+use tokio::{
+    net::TcpListener,
+    sync::{mpsc, oneshot},
+};
+
+use crate::{error::RbitError, file::FileBlock, HashPiece, PeerAddr};
 
 pub struct PeerId(pub [u8; 20]);
 
@@ -62,26 +79,24 @@ enum Message {
     },
 }
 
-use Message::*;
-
 impl Message {
     fn encode(self, buff: &mut Vec<u8>) {
         match self {
-            KeepAlive => buff.extend_from_slice(&[0, 0, 0, 0]),
-            Choke => buff.extend_from_slice(&[0, 0, 0, 1, 0]),
-            Unchoke => buff.extend_from_slice(&[0, 0, 0, 1, 1]),
-            Interested => buff.extend_from_slice(&[0, 0, 0, 1, 2]),
-            NotIntersted => buff.extend_from_slice(&[0, 0, 0, 1, 3]),
-            Have { piece } => {
+            Message::KeepAlive => buff.extend_from_slice(&[0, 0, 0, 0]),
+            Message::Choke => buff.extend_from_slice(&[0, 0, 0, 1, 0]),
+            Message::Unchoke => buff.extend_from_slice(&[0, 0, 0, 1, 1]),
+            Message::Interested => buff.extend_from_slice(&[0, 0, 0, 1, 2]),
+            Message::NotIntersted => buff.extend_from_slice(&[0, 0, 0, 1, 3]),
+            Message::Have { piece } => {
                 buff.extend_from_slice(&[0, 0, 0, 5, 4]);
                 buff.extend_from_slice(&u32_to_bytes(piece));
             }
-            Bitfield { pieces } => {
+            Message::Bitfield { pieces } => {
                 buff.extend_from_slice(&u32_to_bytes(1 + pieces.len() as u32));
                 buff.push(5);
                 buff.extend_from_slice(&pieces);
             }
-            Request {
+            Message::Request {
                 index,
                 begin,
                 length,
@@ -91,7 +106,7 @@ impl Message {
                 buff.extend_from_slice(&u32_to_bytes(begin));
                 buff.extend_from_slice(&u32_to_bytes(length));
             }
-            Piece {
+            Message::Piece {
                 index,
                 begin,
                 block,
@@ -102,7 +117,7 @@ impl Message {
                 buff.extend_from_slice(&u32_to_bytes(begin));
                 buff.extend_from_slice(&block);
             }
-            Cancel {
+            Message::Cancel {
                 index,
                 begin,
                 length,
@@ -117,7 +132,7 @@ impl Message {
 
     fn decode(raw: &[u8]) -> Option<Self> {
         if raw.is_empty() {
-            return Some(KeepAlive);
+            return Some(Message::KeepAlive);
         }
 
         let id = raw[0];
@@ -125,26 +140,26 @@ impl Message {
         let content = &raw[1..];
 
         match (id, len) {
-            (0, 1) => Choke,
-            (1, 1) => Unchoke,
-            (2, 1) => Interested,
-            (3, 1) => NotIntersted,
+            (0, 1) => Message::Choke,
+            (1, 1) => Message::Unchoke,
+            (2, 1) => Message::Interested,
+            (3, 1) => Message::NotIntersted,
             (4, 5) => {
                 let piece = bytes_to_u32(content);
 
-                Have { piece }
+                Message::Have { piece }
             }
             (5, len) if len > 1 => {
                 let pieces = content.into();
 
-                Bitfield { pieces }
+                Message::Bitfield { pieces }
             }
             (6, 13) => {
                 let index = bytes_to_u32(&content[0..4]);
                 let begin = bytes_to_u32(&content[4..8]);
                 let length = bytes_to_u32(&content[8..12]);
 
-                Request {
+                Message::Request {
                     index,
                     begin,
                     length,
@@ -155,7 +170,7 @@ impl Message {
                 let begin = bytes_to_u32(&content[4..8]);
                 let block = content[8..].into();
 
-                Piece {
+                Message::Piece {
                     index,
                     begin,
                     block,
@@ -166,7 +181,7 @@ impl Message {
                 let begin = bytes_to_u32(&content[4..8]);
                 let length = bytes_to_u32(&content[8..12]);
 
-                Cancel {
+                Message::Cancel {
                     index,
                     begin,
                     length,
@@ -175,5 +190,210 @@ impl Message {
             _ => return None,
         }
         .into()
+    }
+}
+
+pub struct RequestedPiece {
+    pub index: u64,
+    pub hash: HashPiece,
+}
+
+pub struct ReceivedPiece {
+    pub index: u64,
+    pub piece: FileBlock,
+}
+
+pub struct MissingPiece {
+    pub index: u64,
+}
+
+pub struct PieceBlockRequest {
+    pub index: u64,
+    pub begin: u64,
+    pub length: u64,
+    receive_block: oneshot::Sender<Option<FileBlock>>,
+}
+
+impl PieceBlockRequest {
+    fn send(self, possible_block: Option<FileBlock>) {
+        let _ = self.receive_block.send(possible_block);
+    }
+
+    pub fn discard(self) {
+        self.send(None)
+    }
+
+    pub fn give(self, block: FileBlock) {
+        self.send(block.into())
+    }
+}
+
+struct SwitchState(AtomicBool);
+
+impl SwitchState {
+    fn new(initial_state: bool) -> Self {
+        Self(AtomicBool::new(initial_state))
+    }
+
+    fn set(&self) {
+        self.0.store(true, Ordering::Release);
+    }
+
+    fn unset(&self) {
+        self.0.store(false, Ordering::Release);
+    }
+
+    fn current(&self) -> bool {
+        self.0.load(Ordering::Acquire)
+    }
+}
+
+struct PeerBitfield {
+    num_pieces: u64,
+    pieces: RwLock<Vec<u8>>,
+}
+
+impl PeerBitfield {
+    fn new(num_pieces: u64) -> Self {
+        let bitfield_length = num_pieces + (8 - num_pieces % 8);
+        let pieces = RwLock::new(vec![0; bitfield_length as usize]);
+
+        Self { num_pieces, pieces }
+    }
+
+    fn has(&self, index: u64) -> Option<bool> {
+        if index >= self.num_pieces {
+            return None;
+        }
+
+        let chunk_index = index / 8;
+        let chunk = { self.pieces.read().unwrap()[chunk_index as usize] };
+        let offset = index % 8;
+        let piece = (chunk >> offset) & 1;
+
+        Some(piece == 1)
+    }
+
+    fn set(&self, index: u64) {
+        if index >= self.num_pieces {
+            return;
+        }
+
+        let chunk_index = index / 8;
+        let offset = index % 8;
+        self.pieces.write().unwrap()[chunk_index as usize] |= 1 << offset;
+    }
+}
+
+pub struct PeerState {
+    choke: SwitchState,
+    interest: SwitchState,
+    bitfield: PeerBitfield,
+}
+
+impl PeerState {
+    fn new(num_pieces: u64) -> Self {
+        Self {
+            choke: SwitchState::new(true),
+            interest: SwitchState::new(false),
+            bitfield: PeerBitfield::new(num_pieces),
+        }
+    }
+
+    fn choke(&self) {
+        self.choke.set();
+    }
+
+    fn unchoke(&self) {
+        self.choke.unset();
+    }
+
+    fn interest(&self) {
+        self.interest.set();
+    }
+
+    fn uninsterest(&self) {
+        self.interest.unset();
+    }
+
+    fn set_piece(&self, index: u64) {
+        self.bitfield.set(index);
+    }
+
+    pub fn choked(&self) -> bool {
+        self.choke.current()
+    }
+
+    pub fn interested(&self) -> bool {
+        self.interest.current()
+    }
+
+    pub fn has_piece(&self, index: u64) -> Option<bool> {
+        self.bitfield.has(index)
+    }
+}
+
+pub struct BasePeer {
+    state: Arc<PeerState>,
+    stop: oneshot::Sender<()>,
+}
+
+impl BasePeer {
+    pub fn stop(self) {
+        let _ = self.stop.send(());
+    }
+}
+
+impl Deref for BasePeer {
+    type Target = Arc<PeerState>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.state
+    }
+}
+
+pub struct PeerClient(BasePeer);
+
+impl PeerClient {
+    pub fn start(
+        address: PeerAddr,
+        num_pieces: u64,
+        requested_pieces: Arc<mpsc::Receiver<RequestedPiece>>,
+        received_pieces: Arc<mpsc::Sender<ReceivedPiece>>,
+        missing_pieces: Arc<mpsc::Sender<MissingPiece>>,
+    ) -> Result<Self, RbitError> {
+        todo!()
+    }
+}
+
+impl Deref for PeerClient {
+    type Target = BasePeer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct PeerServer(BasePeer);
+
+impl PeerServer {
+    pub fn start(
+        stream: TcpListener,
+        num_pieces: u64,
+        request_pieces: Arc<mpsc::Sender<PieceBlockRequest>>,
+    ) -> Result<Self, RbitError> {
+        todo!()
+    }
+
+    pub fn set_piece(&self, index: u64) {
+        self.state.set_piece(index);
+    }
+}
+
+impl Deref for PeerServer {
+    type Target = BasePeer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
