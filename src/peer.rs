82e30fcbcca7cafdad348855ee421661c87ad1cb
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-04-24 09:50:11 +0100

    remove println

--- src/peer.rs
+++ src/peer.rs
@@ -690,7 +690,6 @@ async fn accepted_handshake(handshake: Arc<Handshake>, stream: &mut TcpStream) -
     let mut handshake_reply = vec![0; raw.len()];
 
     if stream.read_exact(&mut handshake_reply).await.is_err() {
-        println!(">>>>>>hi");
         return false;
     }
 
